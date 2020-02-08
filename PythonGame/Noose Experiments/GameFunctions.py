143a08113d424c44509737768926e59df709e6ec
james grappa <horizonation@gmx.de>
2020-02-08 23:49:41 +0100

    movement

--- PythonGame/Noose Experiments/GameFunctions.py
+++ PythonGame/Noose Experiments/GameFunctions.py
@@ -6,25 +6,27 @@ def check_events(P1):
     for event in pg.event.get():
         if event.type==pg.QUIT:
             sys.exit()
+        
+        AccelMoveValue = 0.1
 
-
-        accel = 0.01
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
-                if P1.speed[0] < 1:
-                    P1.speed[0]+=accel
+            if event.key == pg.K_UP:
+                P1.pressedkeys[0]=True
             if event.key == pg.K_LEFT:
-                if P1.speed[0] > -1:
-                    P1.speed[0]-=accel
-            if event.key == pg.K_UP: 
-                if P1.speed[1] > -1:
-                    P1.speed[1]-=accel
-            if event.key == pg.K_DOWN:
-                if P1.speed[1] < 1:
-                    P1.speed[1]+=accel
-  #      if event.type == pg.KEYUP:
- #           if event.key == pg.RIGHT:
-#                decelL
+                P1.pressedkeys[1]=True
+            if event.key == pg.K_DOWN: 
+                P1.pressedkeys[2]=True
+            if event.key == pg.K_RIGHT:
+                P1.pressedkeys[3]=True
+        if event.type == pg.KEYUP:
+           if event.key == pg.K_RIGHT:
+                P1.pressedkeys[3]=False
+           if event.key == pg.K_LEFT:
+               P1.pressedkeys[1]=False
+           if event.key == pg.K_UP: 
+               P1.pressedkeys[0]=False
+           if event.key == pg.K_DOWN:
+               P1.pressedkeys[2]=False
 
 
 def update_screen(GS, screen, P1):
--- PythonGame/Noose Experiments/classfile.py
+++ PythonGame/Noose Experiments/classfile.py
@@ -9,13 +9,14 @@ class GameSettings():
         self.bg_color = (50,50,50)
 
 class Creature():
-    def __init__(self, screen, image, startpos:tuple=(0,0),speed:tuple=(0.0)):
+    def __init__(self, screen, image, startpos:list=[0,0],speed:list=[0.0]):
         self.screen = screen
         self.image = image
         self.rect = self.image.get_rect()
         #position and movement
         self.pos=list(startpos)
         self.speed = list(speed)
+        self.maxspeed = 2
         self.rect.centerx = startpos[0]
         self.rect.centery = startpos[1]
         
@@ -27,18 +28,81 @@ class Creature():
             self.rect.centery = int(self.pos[1])
             if self.rect.centerx > 1024:
                self.rect.centerx = 0
+               self.pos[0] = 0
             elif self.rect.centerx <0:
                 self.rect.centerx = 1024
+                self.pos[0] = 1024
             elif self.rect.centery > 768:
                 self.rect.centery = 0
+                self.pos[1] = 0
             elif self.rect.centery < 0:
                 self.rect.centery = 768
+                self.pos[1] = 768
     
     def blitme(self):
-
         self.screen.blit(self.image, self.rect)
         
 
 class Player(Creature):
-    pass
+    #MaxWalkSpeed
+    MWS=2
+    ACCEL=0.01
+    def __init__(self, *args):
+        #up left down right
+        self.pressedkeys = [False]*4
+        super().__init__(*args)
+       
+    def update(self):
+        #key handlings
+        if self.pressedkeys[0] == True:       
+            if self.speed[1] > self.MWS*-1:
+                self.speed[1] -= self.ACCEL
+        elif self.speed[1] < 0:
+            if self.speed[1] > self.ACCEL*-1:
+                self.speed[1] = 0;
+            else: self.speed[1] += self.ACCEL
+        if self.pressedkeys[1] == True:       
+            if self.speed[0] > self.MWS*-1:
+                self.speed[0] -= self.ACCEL
+        elif self.speed[0] < 0 :
+            if self.speed[0] > self.ACCEL*-1:
+                self.speed[0] = 0;
+            else: self.speed[0] += self.ACCEL
+        if self.pressedkeys[2] == True:       
+            if self.speed[1] < self.MWS:
+                self.speed[1] += self.ACCEL
+        elif self.speed[1] > 0 :
+            if self.speed[1] < self.ACCEL:
+                self.speed[1] = 0;
+            else: self.speed[1] -= self.ACCEL
+        if self.pressedkeys[3] == True:       
+            if self.speed[0] < self.MWS:
+                self.speed[0] += self.ACCEL
+        elif self.speed[0] > 0 :
+            if self.speed[0] < self.ACCEL*-1:
+                self.speed[0] = 0;
+            else:self.speed[0] -= self.ACCEL
+
+        #moving
+        if self.speed != [0,0]:
+            self.pos[0]+=self.speed[0]
+            self.pos[1]+=self.speed[1]
+            self.rect.centerx = int(self.pos[0])
+            self.rect.centery = int(self.pos[1])
+            if self.rect.centerx > 1024:
+               self.rect.centerx = 0
+               self.pos[0] = 0
+            elif self.rect.centerx <0:
+                self.rect.centerx = 1024
+                self.pos[0] = 1024
+            elif self.rect.centery > 768:
+                self.rect.centery = 0
+                self.pos[1] = 0
+            elif self.rect.centery < 0:
+                self.rect.centery = 768
+                self.pos[1] = 768
+            
+
+   
+
 
