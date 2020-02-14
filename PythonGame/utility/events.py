91258f059520a8c82c57a091718876614b3496d0
Riku <riku.antell@gmail.com>
2020-02-14 16:01:41 +0200

    Quick implementation of static event manager
    How to:
    - Listen events by calling `Event.listen()` with event type you want to listen, and with function to be called when the event triggers
    - Trigger event by calling `Event.trigger()` with event type you want to trigger. This calls all functions currently listening to this event.

--- PythonGame/utility/events.py
+++ PythonGame/utility/events.py
@@ -3,15 +3,17 @@ from types import FunctionType as Function
 
 
 class EVENT_TYPE:
-    """Contains list of events that affect wide array objects across the program.
-    Add more if needed."""
+    """Contains events that affect wide array objects across the program.
+    Add more if needed.
+    """
     PLAYER_SPAWN = 1
     PLAYER_DIE = 2
 
 
 class Event():
     """Static event manager that uses bit more memory to avoid polling events every
-    single frame"""
+    single frame
+    """
 
     # Dictionary of sets, use sets because they are faster when removing data
     events = dict()
@@ -19,20 +21,30 @@ class Event():
     @classmethod
     def listen(event: EVENT_TYPE, callable: Function):
         """Add function to events dict, which will be called when the event is
-        triggered"""
-        pass
+        triggered.
+
+        NOTE: Always add *args and **kwargs to listened function
+        """
+        if event not in cls.events:
+            cls.events[event] = set(callable)
+        else:
+            cls.events[event].add(callable)
 
     @classmethod
     def trigger(event: EVENT_TYPE, *args, **kwargs):
         """Triggers event and calls all the stored functions with args and kwargs"""
-        pass
+        if event in cls.events:
+            for callable in cls.events.values():
+                callable(*args, **kwargs)
 
     @classmethod
     def remove(event: EVENT_TYPE, callable: Function):
         """Removes given function from listened events"""
-        pass
+        if event in cls.events:
+            cls.events[event].remove(callable)
 
     @classmethod
     def remove_all(event: EVENT_TYPE):
         """Clears all listened events of specific type"""
-        pass
+        if event in cls.events:
+            cls.events[event] = set()
